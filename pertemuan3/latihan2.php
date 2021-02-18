c488108e2d6bb1ab83c84bd7e3595721db8bf9fe
arilfajri <arielmunchen60@gmail.com>
2021-02-18 15:15:28 +0800

    menyelesaikan pertemuan 3

---
+++ pertemuan3/latihan2.php
@@ -0,0 +1,27 @@
+<?php 
+/*
+Aril Fajri Tolani
+203040013
+https://github.com/arilfajri/pw2021_203040013
+pertemuan 3 - 18 Februari 2021
+mempelajari mengenai struktur kendali PHP 
+*/
+?> 
+
+<?php
+// pengkondisian/percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x=2;
+if($x<20){
+    echo "benar";
+}elseif($x==20){
+    echo "bingo!";
+}
+else{
+    echo "salah";
+}
+?>
