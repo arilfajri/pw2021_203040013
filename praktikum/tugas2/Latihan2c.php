fe191edce2996e39f0debaf50c25700cefd1586d
arilfajri <arielmunchen60@gmail.com>
2021-03-14 21:24:42 +0800

    menyelesaikan praktikum pertemuan 2

---
+++ praktikum/tugas2/Latihan2c.php
@@ -0,0 +1,38 @@
+<?php
+function tumpukanBola($tumpukan){
+    for ($i = 1; $i <= $tumpukan; $i++) {
+        for ($j = 1; $j <= $i; $j++) {
+            echo "<div class=bola>$i</div>";
+        }
+        echo "<br>";
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2c</title>
+    <style>
+        .bola {
+            background-color: salmon;
+            height: 30px;
+            width: 30px;
+            line-height: 30px;
+            text-align: center;
+            margin-bottom: 3px;
+            margin-right: 3px;
+            border-radius: 50%;
+            display: inline-block;
+            border: 2px solid black;
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+<?php echo tumpukanBola(5);?>
+</body>
+</html>
---
+++ praktikum/tugas2/Latihan2d.php
@@ -0,0 +1,37 @@
+<?php
+function hitungDeterminan($a, $b, $c, $d){
+    //baris code perhitungan determinan
+    $matriks = (($a*$d)-($b*$c));
+    //menampilkan matriks
+    echo "<table cellpadding='5' cellspacing='5'>
+        <tr>
+            <td>$a</td>
+            <td>$b</td>
+        </tr>
+        <tr>
+            <td>$c</td>
+            <td>$d</td>
+        </tr>
+    </table>";
+    echo "<br>";
+    echo "<b>Determinan dari matriks tersebut adalah <b>" . $matriks; //menampilkan teks dibawah matriks
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2d</title>
+    <style>
+        table {
+            border-left: 2px solid black;
+            border-right: 2px solid black;
+        }
+    </style>
+</head>
+<body>
+    <?= hitungDeterminan(1,2,3,4);?>
+</body>
+</html>
